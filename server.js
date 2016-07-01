f267290ce03a1e8962f2dc5bc4adb77efa43d6d1
Dimsy <orenmagic@protonmail.ch>
2016-07-01 15:50:07 -0400

    Log added

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var io = require('socket.io').listen(8081);
 
 io.sockets.on('connection', function (socket) {
 	socket.on('hello', function (data){
-		console.log(data);
 		socket.broadcast.send('Connected: '+data);
+		console.log(data);
 	});
 	socket.on('message', function (msg) 
 	{
 		socket.broadcast.send(msg);
 		socket.send(msg);
+		console.log(msg);
 	});
 });
 
